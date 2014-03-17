cb17801f2faf1766e4b038961d4d6b63717b0cc5
Donatas <donce.lt@gmail.com>
2014-03-17 12:24:30 +0100

    cookie clicker just become facebook app

--- cookie_clicker/views.py
+++ cookie_clicker/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+# def something(request):
+#     msg = request.websock.wait()
+#     request.websocket.send(message)
 
 def home(request):
     return render(request, 'home.html')
