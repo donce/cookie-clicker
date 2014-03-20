c6eebd98c6cb10ff13088abec85483d70f62fde8
Donatas <donce.lt@gmail.com>
2014-03-20 16:16:22 +0100

    Some refactoring.

--- cookie_clicker/static/script.js
+++ cookie_clicker/static/script.js
@@ -30,15 +30,11 @@ function moveInterval(current, add, end) {
 
 
 function Friend(data) {
-    console.log('creating friend');
     this.name = data.name;
     this.image = data.picture.data.url;
 
     this.bitmap = new Image();
     this.bitmap.src = this.image;
-
-    this.item = $('<span class="friend_item"><img src="' + this.image + '"/></span>');
-    this.item = "a";
 
     if (false) {
         this.mx = Math.random() * 20;
@@ -72,19 +68,25 @@ function Aquarium() {
     all_friends = [];
 
     this.init = function() {
-        console.log("initing");
+        //TODO: make class fields
         width = $(document).width();
         height = $(document).height();
         canvas = $("#canvas")[0];
         ctx = canvas.getContext("2d");
-        console.log("Setting size to ", width, height);
         ctx.canvas.width = width;
         ctx.canvas.height = height;
     }
 
+    this.drawBackground = function() {
+        ctx.beginPath();
+        ctx.rect(0, 0, width, height);
+        ctx.fillStyle = "blue";
+        ctx.fill();
+    }
+
     this.update = function() {
-        console.log("update")
-        this.all_friends.forEach(function(friend) {
+        aquarium.drawBackground();
+        all_friends.forEach(function(friend) {
             friend.move();
             ctx.drawImage(friend.bitmap, friend.px, friend.py);
         });
@@ -93,8 +95,8 @@ function Aquarium() {
 }
 
 aquarium = new Aquarium();
-$(window).load(function() {
 
+$(window).load(function() {
   window.fbAsyncInit = function() {
   FB.init({
     appId      : '189451487932004',
@@ -108,20 +110,8 @@ $(window).load(function() {
     if (response.status === 'connected') {
         getFriends();
     } else if (response.status === 'not_authorized') {
-      // In this case, the person is logged into Facebook, but not into the app, so we call
-      // FB.login() to prompt them to do so.
-      // In real-life usage, you wouldn't want to immediately prompt someone to login
-      // like this, for two reasons:
-      // (1) JavaScript created popup windows are blocked by most browsers unless they
-      // result from direct interaction from people using the app (such as a mouse click)
-      // (2) it is a bad experience to be continually prompted to login upon page load.
       FB.login();
     } else {
-      // In this case, the person is not logged into Facebook, so we call the login()
-      // function to prompt them to do so. Note that at this stage there is no indication
-      // of whether they are logged into the app. If they aren't then they'll see the Login
-      // dialog right after they log in to Facebook.
-      // The same caveats as above apply to the FB.login() call here.
       FB.login();
     }
   });
@@ -129,33 +119,8 @@ $(window).load(function() {
     aquarium.init();
 });
 
-current = 0;
 
 function createFriend(data) {
     var friend = new Friend(data);
     all_friends.push(friend);
-    $('#friends_list').append(friend.item);
 }
-
-  // Load the SDK asynchronously
-  (function(d){
-   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   ref.parentNode.insertBefore(js, ref);
-  }(document));
-
-/*
-  function testAPI() {
-    FB.api('/me', function(response) {
-      console.log('Good to see you, ' + response.name + '.');
-        console.log(response);
-    });
-
-    FB.api('me/friends?fields=picture,name', function(response) {
-        response.data.forEach(createFriend);
-        setInterval(aquarium.update, 1);
-    });
-  }
-*/
